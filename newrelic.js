21713fd5bee8fef2917c555c4ad7b31f9292a39b
Tim Officer <timothyofficer@gmail.com>
2014-09-07 20:51:53 -0400

    adding new relic and gitignore

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['Personal Website'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : 'e7c5a21591fb5d706825572ef8cde21bb7cde86c',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'info'
+  }
+};
